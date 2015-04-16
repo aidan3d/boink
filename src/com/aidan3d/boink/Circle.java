7a567715b8b77ae7be7e1db61179a88a6334a22e
aidanhegarty <aidanhegarty@yahoo.com>
2015-04-16 02:24:26 -0400

    The Circle class now has the ability to update its position in the game window.

--- src/com/aidan3d/boink/Circle.java
+++ src/com/aidan3d/boink/Circle.java
@@ -19,17 +19,28 @@ import math.geom2d.Vector2D;
 public class Circle {
     protected Vector2D center;  // the circle's center
     protected double radius;    // the circles radius
-
+    
+    /**
+     * The no-argument constructor.
+     */
     public Circle() {
         center = new Vector2D(0.0F, 0.0F);
         radius = 0.0F;
     }
 
+    
+    /**
+     * The two-argument constructor
+     * @param c a 2d Euclidian vector referencing the
+     * circle's center
+     * @param r the circle's radius
+     */
     public Circle(Vector2D c, double r) {
         center = c;
         radius = r;
     }
-    
+
+
     /**
      * This method paints a circle to screen.
      * @param buf the backbuffer
@@ -38,5 +49,14 @@ public class Circle {
         buf.drawOval(0, 0, 100, 100);
     }
 
+
+    /**
+     * This method sets the circle in the right
+     * position in the game window before we
+     * call the draw() method.
+     */
+    public void update() {
     
+    }
+
 }
